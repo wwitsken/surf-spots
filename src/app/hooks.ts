869554256ddde67624f78ea008e76b7420cb85a1
wwitsken <witsken.w@gmail.com>
2024-03-27 11:46:23 -0700

    Add Firebase dependencies and update Redux hooks

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -5,8 +5,9 @@
 // for importing and re-exporting the typed versions of hooks.
 /* eslint-disable @typescript-eslint/no-restricted-imports */
 import { useDispatch, useSelector } from "react-redux"
+import type { TypedUseSelectorHook } from "react-redux"
 import type { AppDispatch, RootState } from "./store"
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
